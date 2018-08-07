7676a82fd6faf6d8a8a436c5e4e085838f10c996
bluepen <31067876+bluepenTeam@users.noreply.github.com>
2018-08-07 23:01:23 +0900

    Add files via upload

---
+++ 끄투온라인_서버리스트/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace 끄투온라인_서버리스트
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kkutu_co_kr = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.veatyou_xyz = new System.Windows.Forms.Button();
+            this.끄투_닷넷 = new System.Windows.Forms.Button();
+            this.prhkt_zz_am = new System.Windows.Forms.Button();
+            this.끄투_닷컴 = new System.Windows.Forms.Button();
+            this.kkutu_cc = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kkutu_co_kr
+            // 
+            this.kkutu_co_kr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.kkutu_co_kr.FlatAppearance.BorderSize = 0;
+            this.kkutu_co_kr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kkutu_co_kr.Font = new System.Drawing.Font("맑은 고딕", 20F);
+            this.kkutu_co_kr.ForeColor = System.Drawing.Color.White;
+            this.kkutu_co_kr.Location = new System.Drawing.Point(3, 3);
+            this.kkutu_co_kr.Name = "kkutu_co_kr";
+            this.kkutu_co_kr.Size = new System.Drawing.Size(247, 139);
+            this.kkutu_co_kr.TabIndex = 0;
+            this.kkutu_co_kr.Text = "끄투코리아";
+            this.kkutu_co_kr.UseVisualStyleBackColor = false;
+            this.kkutu_co_kr.Click += new System.EventHandler(this.kkutu_co_kr_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.veatyou_xyz, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.끄투_닷넷, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.prhkt_zz_am, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.끄투_닷컴, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.kkutu_cc, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.kkutu_co_kr, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 159);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(760, 290);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // veatyou_xyz
+            // 
+            this.veatyou_xyz.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.veatyou_xyz.FlatAppearance.BorderSize = 0;
+            this.veatyou_xyz.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.veatyou_xyz.Font = new System.Drawing.Font("맑은 고딕", 20F);
+            this.veatyou_xyz.ForeColor = System.Drawing.Color.White;
+            this.veatyou_xyz.Location = new System.Drawing.Point(3, 148);
+            this.veatyou_xyz.Name = "veatyou_xyz";
+            this.veatyou_xyz.Size = new System.Drawing.Size(247, 139);
+            this.veatyou_xyz.TabIndex = 5;
+            this.veatyou_xyz.Text = "비트유끄투";
+            this.veatyou_xyz.UseVisualStyleBackColor = false;
+            this.veatyou_xyz.Click += new System.EventHandler(this.veatyou_xyz_Click);
+            // 
+            // 끄투_닷넷
+            // 
+            this.끄투_닷넷.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.끄투_닷넷.FlatAppearance.BorderSize = 0;
+            this.끄투_닷넷.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.끄투_닷넷.Font = new System.Drawing.Font("맑은 고딕", 20F);
+            this.끄투_닷넷.ForeColor = System.Drawing.Color.White;
+            this.끄투_닷넷.Location = new System.Drawing.Point(256, 148);
+            this.끄투_닷넷.Name = "끄투_닷넷";
+            this.끄투_닷넷.Size = new System.Drawing.Size(247, 139);
+            this.끄투_닷넷.TabIndex = 4;
+            this.끄투_닷넷.Text = "끄투닷넷";
+            this.끄투_닷넷.UseVisualStyleBackColor = false;
+            this.끄투_닷넷.Click += new System.EventHandler(this.끄투_닷넷_Click);
+            // 
+            // prhkt_zz_am
+            // 
+            this.prhkt_zz_am.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.prhkt_zz_am.FlatAppearance.BorderSize = 0;
+            this.prhkt_zz_am.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.prhkt_zz_am.Font = new System.Drawing.Font("맑은 고딕", 20F);
+            this.prhkt_zz_am.ForeColor = System.Drawing.Color.White;
+            this.prhkt_zz_am.Location = new System.Drawing.Point(509, 148);
+            this.prhkt_zz_am.Name = "prhkt_zz_am";
+            this.prhkt_zz_am.Size = new System.Drawing.Size(247, 139);
+            this.prhkt_zz_am.TabIndex = 3;
+            this.prhkt_zz_am.Text = "Prj_KT";
+            this.prhkt_zz_am.UseVisualStyleBackColor = false;
+            this.prhkt_zz_am.Click += new System.EventHandler(this.prhkt_zz_am_Click);
+            // 
+            // 끄투_닷컴
+            // 
+            this.끄투_닷컴.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.끄투_닷컴.FlatAppearance.BorderSize = 0;
+            this.끄투_닷컴.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.끄투_닷컴.Font = new System.Drawing.Font("맑은 고딕", 20F);
+            this.끄투_닷컴.ForeColor = System.Drawing.Color.White;
+            this.끄투_닷컴.Location = new System.Drawing.Point(509, 3);
+            this.끄투_닷컴.Name = "끄투_닷컴";
+            this.끄투_닷컴.Size = new System.Drawing.Size(247, 139);
+            this.끄투_닷컴.TabIndex = 2;
+            this.끄투_닷컴.Text = "끄투닷컴";
+            this.끄투_닷컴.UseVisualStyleBackColor = false;
+            this.끄투_닷컴.Click += new System.EventHandler(this.끄투_닷컴_Click);
+            // 
+            // kkutu_cc
+            // 
+            this.kkutu_cc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.kkutu_cc.FlatAppearance.BorderSize = 0;
+            this.kkutu_cc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kkutu_cc.Font = new System.Drawing.Font("맑은 고딕", 20F);
+            this.kkutu_cc.ForeColor = System.Drawing.Color.White;
+            this.kkutu_cc.Location = new System.Drawing.Point(256, 3);
+            this.kkutu_cc.Name = "kkutu_cc";
+            this.kkutu_cc.Size = new System.Drawing.Size(247, 139);
+            this.kkutu_cc.TabIndex = 1;
+            this.kkutu_cc.Text = "끄투한국";
+            this.kkutu_cc.UseVisualStyleBackColor = false;
+            this.kkutu_cc.Click += new System.EventHandler(this.kkutu_cc_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(27)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(-6, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(796, 32);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(72)))), ((int)(((byte)(54)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(761, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 20);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(10, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "끄투온라인 서버리스트 V1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(4, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "만든이 : 블루펜";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "끄투온라인 서버리스트 V1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button kkutu_co_kr;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button prhkt_zz_am;
+        private System.Windows.Forms.Button 끄투_닷컴;
+        private System.Windows.Forms.Button kkutu_cc;
+        private System.Windows.Forms.Button veatyou_xyz;
+        private System.Windows.Forms.Button 끄투_닷넷;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 끄투온라인_서버리스트/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 끄투온라인_서버리스트
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Internet(string kkutu)
+        {
+            System.Diagnostics.Process.Start(kkutu);
+        }
+        #region 사이트
+        private void kkutu_cc_Click(object sender, EventArgs e)
+        {
+            Internet("http://kkutu.cc");
+        }
+
+        private void kkutu_co_kr_Click(object sender, EventArgs e)
+        {
+            Internet("https://kkutu.co.kr");
+        }
+
+        private void 끄투_닷컴_Click(object sender, EventArgs e)
+        {
+            Internet("http://끄투.닷컴");
+        }
+
+        private void 끄투_닷넷_Click(object sender, EventArgs e)
+        {
+            Internet("http://끄투.닷넷");
+        }
+
+        private void veatyou_xyz_Click(object sender, EventArgs e)
+        {
+            Internet("https://veatyou.xyz");
+        }
+
+        private void prhkt_zz_am_Click(object sender, EventArgs e)
+        {
+            Internet("https://prjkt.zz.am");
+        }
+        #endregion
+        #region 창이동
+        private Point mousePoint;
+private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousePoint = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                Location = new Point(this.Left - (mousePoint.X - e.X),
+                     this.Top - (mousePoint.Y - e.Y));
+            }
+        }
+        #endregion
+        #region 종료
+        private void button1_Click(object sender, EventArgs e) //프로그램을 종료합니다
+        {
+            Application.Exit();
+            Application.ExitThread();
+            Environment.Exit(0);
+        }
+        #endregion
+    }
+}
